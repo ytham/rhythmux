b65c0fc34cb335e69549f0f8298e189a5faeffb5
Yu Jiang Tham <yoojbruin@gmail.com>
2013-05-12 16:57:28 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rhythmux::Application.config.secret_token = '0a1765213a6449a9b49edd5c113dec6ac352ca1f02ca102842687299d62996d9a180c8c1d824ce1cdda3e50ec06fe1c4595186080c5913e37c07b280835b62e8'
